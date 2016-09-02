6f0a0350fc45e1b9ce37ec6d5999c4a14e7a4770
cjwalle97 <cjwalle97@gmail.com>
2016-09-02 10:47:29 -0500

    Add files via upload

---
+++ LoopsDecrement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+//Using a for loop to iterate backward, print the values 100 to 0.
+
+
+int main()
+{
+	//- Modify the for loop to include 0 and include 100
+	for (int i = 100; i >= 0; i--)
+		std::cout << i << std::endl;
+	system("pause");
+	return 0;
+}
+//- Modify the for loop to include 0 and not 100
+//for (int i = 99; i >= 0; i--)
+
+//- Modify the for loop to decrement by 2
+//for (int i = 100; i >= 0; i -= 2)
+
+ //- Modify the for loop to not include 0 and include 100
+ //for (int i = 100; i > 0; i--)
